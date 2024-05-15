e0db8c9e457d6eb4620164772003d0182c95aeae
Ian Jarvis <ijarvis12@gmail.com>
2024-05-15 10:47:27 -0500

    Update exwm-init.el

--- .emacs.d/exwm-init.el
+++ .emacs.d/exwm-init.el
@@ -1,9 +1,9 @@
 ;; install exwm with M-x package-install RET exwm RET
 (require 'exwm)
 (require 'exwm-config)
-(exwm-config-default)
 (require 'exwm-systemtray)
-(exwm-systemtray-enable)
 (require 'exwm-randr)
+(exwm-config-default)
+(exwm-systemtray-enable)
 (exwm-randr-enable)
 (setq exwm-workspace-number 8)
